74d9bca44d127a82c2bf29c7d7b81a6d9c7852c2
Philip Betzler <pb060@hdm-stuttgart.de>
2017-10-15 19:23:10 +0200

    Inserted funktional Camera Movement
    [added] Ground Material
    [changed] CameraScript
    [deleted] --

--- Assets/Scripts/MouseCamMove.cs
+++ Assets/Scripts/MouseCamMove.cs
@@ -6,79 +6,26 @@ using UnityEngine;
 
 /// <summary>
 /// class for main camera movement
+/// Last Changed by Philip on 15.10.2017
 /// </summary>
 public class MouseCamMove : MonoBehaviour
 {
-    //private var for parant object of camera and boxes
-    [SerializeField] private GameObject camParent;
+    public float speed;
+    // Use this for initialization
+    void Start()
+    {
 
-    /// <summary>
-    /// calls MouseMove
-    /// </summary>
-    void Update ()
-	{
-	    MouseMove();
     }
 
-
-    /// <summary>
-    /// It moves the parentobject with
-    /// cam and boxes in a direction
-    /// </summary>
-    /// <param name="other">gameobject it collides with</param>
-    //TODO 
-    //TODO Won't go in Trigger
-    //TODO
-    void OnTriggerEnter(Collider other)
+    // Update is called once per frame
+    void Update()
     {
-        //TODO 
-        //TODO parentobject wont move
-        //TODO
-        if (other.gameObject.CompareTag("Up"))
+        if (Input.GetAxis("Mouse X") != 0.0f)
         {
-            other.gameObject.transform.position += new Vector3(0,0,1);
-            Console.WriteLine("Up");
+            transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed, 0.0f,
+                Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed);
         }
-        if (other.gameObject.CompareTag("Down"))
-        {
-            other.gameObject.transform.position += new Vector3(0, 0, -1);
-            Console.WriteLine("Down");
 
-        }
-        if (other.gameObject.CompareTag("Left"))
-        {
-            other.gameObject.transform.position += new Vector3(-1, 0, 0);
-            Console.WriteLine("Left");
-
-        }
-        if (other.gameObject.CompareTag("Right"))
-        {
-            other.gameObject.transform.position += new Vector3(1, 0, 0);
-            Console.WriteLine("Right");
-
-        }
-    }
-
-    /// <summary>
-    /// casts a ray and sets the position of
-    /// the gameobject this is attached to
-    /// to the position of the mouse
-    /// </summary>
-    private void MouseMove()
-    {
-        //TODO 
-        //TODO ball laggs behind
-        //TODO sync only every frame
-        //TODO FixedUpdate would be even later
-        //TODO
-        RaycastHit hitInfo;
-        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(mouseRay, out hitInfo))
-        {
-            Vector3 positionMouse = hitInfo.point;
-            positionMouse.y = 0.5f;
-            this.transform.position = positionMouse;
-        }
     }
 
 }
